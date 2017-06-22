005dfd119c638354abc64170cf12c4ee5a67b348
Magesh <tkmagesh77@gmail.com>
2017-06-22 16:25:26 +0530

    Day-01 Complete

--- 01-Language/src/calculator.js
+++ 01-Language/src/calculator.js
@@ -1,16 +1,8 @@
 function add(x,y){
 	function parseArg(n){
-		if (Array.isArray(n)){
-			var result = 0;
-			for(var index = 0; index < n.length; index++)
-				result += parseArg(n[index]);
-			return result;
-		}
+		if (Array.isArray(n)) return add.apply(this, n)
 		if (typeof n === 'function') return parseArg(n());
 		return isNaN(n) ? 0 : parseInt(n,10);
 	}
-	var result = 0;
-	for(var index=0; index < arguments.length; index++)
-		result += parseArg(arguments[index]);
-	return result;
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
